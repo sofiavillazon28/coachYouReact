de1323d1defed3f331fdd65dd1ca262545d77083
Victor Villazón <victorzn23@gmail.com>
2023-02-08 17:28:32 -0500

    feat: new config with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import "../src/styles/css/index2.css"
-import { primaryRoute } from '../src/routes/PrimaryRoute';
 import { RouterProvider } from "react-router-dom";
+
+import { primaryRoute } from '../src/routes/PrimaryRoute';
+
+import "../src/styles/scss/index.scss"
 
 function App() {
   return (
-    <>
-      <RouterProvider router={primaryRoute} />
-    </>
+    <RouterProvider router={primaryRoute} />
   );
 }
 
