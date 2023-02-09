11e87b9f0caf4a15310bc1e9d5a8df38a730b283
Victor Villazón <victorzn23@gmail.com>
2023-02-09 17:09:55 -0500

    feat: update header

--- src/components/PrincipalSections/Header.jsx
+++ src/components/PrincipalSections/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+
 import searchIcon from "../../assets/images/icons/search.svg"
 import logo from "../../assets/images/logo/logo-1.svg"
 
@@ -8,23 +9,23 @@ const Header = () => {
       <nav id="header-nav" className="nav">
         <div className="container px-15 flex justify-between items-center p-relative md:px-0">
           <div className="flex  gap-xl">
-            <a href="/">
-              <img src={logo} alt="" className="nav__logo" width="114" height="39" />
-            </a>
+            <Link to="/">
+              <img src={logo} alt="Logo CoachYou" className="nav__logo" width="114" height="39" />
+            </Link>
             <ul className="list flex items-center gap-xl hidden md:flex">
               <li>
-                <a className="list__link p2" href="/">Home</a>
+                <Link className="list__link p2" to="/">Home</Link>
               </li>
               <li>
-                <a className="list__link p2" href="/list">Couches</a>
+                <Link className="list__link p2" to="/list">Couches</Link>
               </li>
             </ul>
           </div>
           <div className="list flex items-center gap-md">
-            <img className="hidden md:flex" src={searchIcon} alt="searchIcon"/>
+              <img className="hidden md:flex" src={searchIcon} alt="searchIcon"/>
               <span className="divider-line h-full"></span>
-              <a className="list__link p3" href="/login">Login</a>
-              <a href="/register"><button className="button-white" href="/register">Join Us</button></a>
+              <Link className="list__link p3" to="/login">Login</Link>
+              <Link to="/register"><button className="button-white">Join Us</button></Link>
           </div>
         </div>
       </nav>
